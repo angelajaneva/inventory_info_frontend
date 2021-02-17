bfa80a984e020ea18af855775ad2184d5b87f899
ristovkiril <ristov.kiril@yahoo.com>
2021-02-17 17:44:43 +0100

    nav items added some css class

--- src/components/Navigation/newMenuTree.js
+++ src/components/Navigation/newMenuTree.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NewMenuTree = (props) => (
-    <li className="nav-link pointer ">
+    <li className="nav-link pointer my-1">
         <input id={props.id}
                className="float-left checkbox-round"
                type="checkbox"
@@ -10,19 +10,19 @@ const NewMenuTree = (props) => (
                checked={props.checked}
                onChange={props.onChange} />
 
-        <a href={"#collapse-" + props.id} className="p-0 my-0 ml-1 clear" data-toggle={'collapse'} aria-expanded={"false"} >
-            <label htmlFor={props.id}>
+        <a href={"#collapse-" + props.id} className="p-0 my-0 ml-1  clear" data-toggle={'collapse'} aria-expanded={"false"} >
+            <label htmlFor={props.id} className="m-0 w-100">
                 <span className="nav-label p-1 m-0 ml-1 pointer">{props.label}</span>
+                <span className="fa float-right"/>
             </label>
-            <span className="fa float-right"/>
         </a>
         {
             props.level === 2 ?
-                <ul className="nav nav-second-level collapse px-0" id={"collapse-" + props.id}>
+                <ul className="nav nav-second-level my-1 collapse px-0" id={"collapse-" + props.id}>
                     {props.children}
                 </ul>
                 :
-                <ul className="nav nav-third-level collapse" id={"collapse-" + props.id}>
+                <ul className="nav nav-third-level my-1 collapse px-0" id={"collapse-" + props.id}>
                     {props.children}
                 </ul>
         }

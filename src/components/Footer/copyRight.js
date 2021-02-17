b93e7cdccc03663d83b38ba2adbed5d25823b8a1
angelajaneva <janeva.angela@live.com>
2021-02-17 15:48:12 +0100

    automatically get year in footer

--- src/components/Footer/copyRight.js
+++ src/components/Footer/copyRight.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => (
   <div className="text-dark">
-      <strong>Copyright</strong> <a href="https://klimatskipromeni.mk/" className="text-dark">klimatskipromeni.mk</a> &copy; 2021
+      <strong>Copyright</strong> <a href="https://klimatskipromeni.mk/" className="text-dark">klimatskipromeni.mk</a> &copy; {(new Date().getFullYear())}
   </div>
 );
